bb6d3541ee09ba9aece6c59f4cc49b887b61151c
RepEpic <6935531+RepEpic@users.noreply.github.com>
2021-09-24 19:15:37 -0700

    more cleanup

--- P4G Save Tool/WPF/MainWindow.xaml.cs
+++ P4G Save Tool/WPF/MainWindow.xaml.cs
@@ -73,41 +73,8 @@ namespace P4G_Save_Tool
             this.Lu = lu;
         }
     }
-
-    public class Item
-    {
-        private string name;
-        private ushort id;
-        public string Name { get { return name; } }
-        public ushort ID { get { return id; } }
-        public Item(string name, ushort id)
-        {
-            this.name = name;
-            this.id = id;
-        }
-    };
-    public class ItemStack
-    {
-        public string Text { get; set; }
-        public ushort ID;
-        public ItemStack(string text, ushort id)
-        {
-            Text = text;
-            ID = id;
-        }
-    };
-    public class ItemByte
-    {
-        private string name;
-        private byte id;
-        public string Name { get { return name; } }
-        public byte ID { get { return id; } }
-        public ItemByte(string name, byte id)
-        {
-            this.name = name;
-            this.id = id;
-        }
-    };
+    
+    
     public class SocialLink
     {
         private string name;
